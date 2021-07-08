a8966cb3f277e68a40d067fa9bb06e0a6322786b
senabi <sensenabi@gmail.com>
2021-07-08 00:35:34 -0500

    base

--- app/app.py
+++ app/app.py
@@ -7,6 +7,8 @@ PORT = 5000
 # initialize flask application
 app = Flask(__name__)
 
+test_data = []
+
 
 # sample hello world page
 @app.route('/')
@@ -17,14 +19,15 @@ def hello():
 # sample api endpoint
 @app.route('/api/test', methods=['GET', 'POST'])
 def test():
+    global test_data
     if request.method == 'POST':
         # get parameters from post request
         parameters = request.get_json()
-        if 'test' in parameters:
-            return jsonify({'value': parameters['test']})
-        return jsonify({'error'})
+        test_data.append(parameters)
+        print(test_data)
+        return jsonify(test_data)
     else:
-        return jsonify({'test': 'success'})
+        return jsonify(test_data)
 
 
 if __name__ == '__main__':
